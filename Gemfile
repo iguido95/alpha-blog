e181a32f0953f0169223e6c7042c5aeb5473a823
Guido Teunissen <iguido@me.com>
2016-04-01 21:26:05 +0200

    Added User validations

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'rails-i18n'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -48,4 +50,3 @@ group :development do
     # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    format:  { with: VALID_EMAIL_REGEX, message: "Must be an email address" }
 
 end
