a4aefc53d2013befc102369da420a3f7588922ff
Guido Teunissen <iguido@me.com>
2016-04-02 12:16:58 +0200

    User registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,12 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
-  validates :email, presence: true, uniqueness: { case_sensitive: false },
+  validates :email, presence: true,
+                    confirmation: true,
+                    uniqueness: { case_sensitive: false },
                     format:  { with: VALID_EMAIL_REGEX, message: "Must be an email address" }
+  # validates :email_confirmation, presence: true
+
+  validates :password, confirmation: true
 
 end
