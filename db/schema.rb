b912aedc60043efa01845a1993542fa44a3e1026
Guido Teunissen <iguido@me.com>
2016-04-03 12:23:02 +0200

    created article-category many-to-many association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402184254) do
+ActiveRecord::Schema.define(version: 20160403101125) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer  "article_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "article_categories", ["article_id"], name: "index_article_categories_on_article_id"
+  add_index "article_categories", ["category_id"], name: "index_article_categories_on_category_id"
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
